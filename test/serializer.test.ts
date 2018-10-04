0733edefc865bf2ba0cee94ee7afa904711d2c15
Lukas Bombach <lukas.bombach@googlemail.com>
2018-10-04 23:18:24 +0200

    asdas

--- test/serializer.test.ts
+++ test/serializer.test.ts
@@ -3,15 +3,15 @@ import ReactSerializer from '../lib/serializer/react';
 
 test('an element', async () => {
   const json = Parser.getJson(`<template><div>contents</div></template>`);
-  expect(ReactSerializer.serialize(json)).toEqual(`React.createElement(div, {}, ["contents"])`)
+  expect(ReactSerializer.serialize(json)).toBe(`React.createElement(div, {}, ["contents"])`)
 });
 
 test('an element with a prop', async () => {
   const json = Parser.getJson(`<template><div foo="bar">contents</div></template>`);
-  expect(ReactSerializer.serialize(json)).toEqual(`React.createElement(div, {"foo":"bar"}, ["contents"])`)
+  expect(ReactSerializer.serialize(json)).toBe(`React.createElement(div, {"foo":"bar"}, ["contents"])`)
 });
 
 test('an element with a child element', async () => {
   const json = Parser.getJson(`<template><div><div>contents</div></div></template>`);
-  expect(ReactSerializer.serialize(json)).toEqual(`React.createElement(div, {}, [React.createElement(div, {}, ["contents"])])`)
+  expect(ReactSerializer.serialize(json)).toBe(`React.createElement(div, {}, [React.createElement(div, {}, ["contents"])])`)
 });
