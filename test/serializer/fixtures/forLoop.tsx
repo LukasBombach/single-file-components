1b86f57b50d6cf5cc5822bfaa0dfa02a788a4a7c
Lukas Bombach <lukas.bombach@googlemail.com>
2019-03-07 16:11:43 +0100

    set up forLoop test

---
+++ test/serializer/fixtures/forLoop.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { ComponentDescriptor } from "../../../lib/model/component";
+
+export const compDescriptor: ComponentDescriptor = {
+  fileName: "ExpectedClass",
+  template: {
+    root: {
+      name: "ul",
+      children: [
+        {
+          name: "li",
+          attrs: { "v-for": "item in items" },
+          children: [{ type: "text", text: "{{item}}" }]
+        }
+      ],
+      attrs: {}
+    }
+  },
+  script: {
+    components: {},
+    data: {
+      items: ["Foo", "Bar"]
+    },
+    props: {}
+  }
+};
+
+export class ExpectedClass extends React.Component {
+  state = { items: ["Foo", "Bar"] };
+
+  render() {
+    return <ul>{this.state.items.map((item, i) => <li key={i}>{item}</li>)}</ul>;
+  }
+}
