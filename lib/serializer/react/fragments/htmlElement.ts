09b2fd74236c60e262b654d7c574a3898f63e12a
Lukas Bombach <lukas.bombach@googlemail.com>
2019-03-18 23:27:35 +0100

    more refactoring

--- lib/serializer/react/fragments/htmlElement.ts
+++ lib/serializer/react/fragments/htmlElement.ts
@@ -1,10 +1,17 @@
-import { Element } from "../../../model/template";
-import Props from "../props";
-import Template from ".";
+import { Element, Props } from "../../../model/template";
 
-export default function htmlElement(el: Element, t: Template): string {
-  const reactEl = `"${el.name}"`;
-  const props = Props.getProps(el.props);
-  const children = !el.children ? "undefined" : `[${el.children.map(c => t.serialize(c)).join(",")}]`;
-  return `React.createElement(${reactEl}, ${props}, ${children})`;
+export default function htmlElement(el: Element, { serialize }): string {
+  const props = !el.props ? "{}" : serializeProps(el.props);
+  const children = !el.children ? "undefined" : `[${el.children.map(c => serialize(c)).join(",")}]`;
+  return `React.createElement("${el.name}", ${props}, ${children})`;
+}
+
+function serializeProps(props: Props): string {
+  return Object.entries(props)
+    .map(([key, val]) => serializePair(key, val))
+    .join(", ");
+}
+
+function serializePair(key: string, val: string): string {
+  return key.charAt(0) === ":" ? `"${key.substring(1)}": ${val}` : `"${key}": "${val}"`;
 }
--- lib/serializer/react/fragments/v-for.ts
+++ lib/serializer/react/fragments/v-for.ts
@@ -1,30 +1,28 @@
 import { Element } from "../../../model/template";
-import Props from "../props";
-import Template from ".";
 
 const regexWithIndex = /\(\W*(\w+)\W*,\W*(\w+)\W*\)\W*in\W*(\w+)/; // matches "(item, index) in items"
 const regexWithoutIndex = /(\w+)\W*in\W*(\w+)/; // matches "item in items"
 
-export default function vFor(el: Element, t: Template): string {
-  if (regexWithIndex.test(el.props["v-for"])) return vForWithKey(el, t);
-  return vForWithoutKey(el, t);
+export default function vFor(el: Element, { serialize }): string {
+  if (regexWithIndex.test(el.props["v-for"])) return vForWithKey(el, serialize);
+  return vForWithoutKey(el, serialize);
 }
 
-function vForWithKey(el: Element, t: Template): string {
+function vForWithKey(el: Element, serialize: Function): string {
   const [, item, key, items] = el.props["v-for"].match(regexWithIndex);
-  return `${items}.map((${item}, ${key}) => ${htmlElementWithoutVFor(el, t)})`;
+  return `${items}.map((${item}, ${key}) => ${htmlElementWithoutVFor(el, serialize)})`;
 }
 
-function vForWithoutKey(el: Element, t: Template): string {
+function vForWithoutKey(el: Element, serialize: Function): string {
   const [, item, items] = el.props["v-for"].match(regexWithoutIndex);
-  return `${items}.map((${item}) => ${htmlElementWithoutVFor(el, t)})`;
+  return `${items}.map((${item}) => ${htmlElementWithoutVFor(el, serialize)})`;
 }
 
-function htmlElementWithoutVFor(el: Element, t: Template): string {
+function htmlElementWithoutVFor(el: Element, serialize: Function): string {
   const reactEl = `"${el.name}"`;
   const attrs = Object.assign({}, el.props);
   delete attrs["v-for"];
   const props = Props.getProps(attrs);
-  const children = !el.children ? "undefined" : `[${el.children.map(c => t.serialize(c)).join(",")}]`;
+  const children = !el.children ? "undefined" : `[${el.children.map(c => serialize(c)).join(",")}]`;
   return `React.createElement(${reactEl}, ${props}, ${children})`;
 }
