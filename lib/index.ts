1de659907728581dd8cf28c675df644ab9581aa3
Lukas Bombach <lukas.bombach@googlemail.com>
2019-03-24 15:18:21 +0100

    index s

--- lib/index.ts
+++ lib/index.ts
@@ -1,8 +1,13 @@
-// import Component from "./parser/component";
-// import ReactSerializer from "./serializer/react";
+import loaderUtils from "loader-utils";
+import Template from "./parser/template";
+import Script from "./parser/script";
+import Style from "./parser/style";
+import Component from "./parser/component";
 
 export default function sfcLoader(source: string): string {
-  // const component = new Component(source);
-  //return new ReactSerializer(component).toString()
-  return source;
+  const { type } = loaderUtils.parseQuery(this.resourceQuery);
+  if (type === "template") return new Template(source).toString();
+  if (type === "script") return new Script(source).toString();
+  if (type === "style") return new Style(source).toString();
+  return new Component(source).toString();
 }
