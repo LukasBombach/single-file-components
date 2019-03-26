b6f3a3bec73ff4d29ce03ce4fa0d8ead9dd0bb57
Lukas Bombach <lukas.bombach@googlemail.com>
2019-03-26 13:39:51 +0100

    code cleanup

--- lib/transpiler/react/component.ts
+++ lib/transpiler/react/component.ts
@@ -1,32 +1,17 @@
 import path from "path";
-import { loader } from "webpack";
 import requireFromString from "require-from-string";
 import camelCase from "camelcase";
-import Component from "../../model/component";
 import ScriptModel from "../../model/script";
-import Parser from "../../parser";
 import Transpiler from "./transpiler";
-import VDom from "../../serializer/react/vdom";
-import Script from "../../parser/script";
 
-interface TemplateVars {
-  name: string;
-  props: string;
-  stateHooks: string;
-  vdom: string;
-}
+// import React, { Component } from "react";
+// export default
 
 export default class ComponentTranspiler extends Transpiler {
   async toString(): Promise<string> {
     const script = await this.getScript();
     const vdom = await this.getVDom();
     const name = this.getName(script.name);
-
-    /* 
-    import React, { Component } from "react";
-    
-      export default 
-    */
 
     return `class ${name} extends Component {
 
@@ -42,22 +27,18 @@ export default class ComponentTranspiler extends Transpiler {
   }
 
   private async getScript(): Promise<ScriptModel> {
-    const transpileScript = await this.loadScript(); // 700ms
+    const transpileScript = await this.loadWith("babel-loader", "sfcLoader?type=script"); // 700ms
     return requireFromString(transpileScript).default; // 1ms
   }
 
-  // TODO Make loadScript, loadTemplate, loadStyle part of the loader context?
-  private loadScript(): Promise<string> {
-    return new Promise((resolve, reject) => {
-      const request = `!babel-loader!sfcLoader?type=script!${this.loader.resourcePath}`;
-      this.loader.loadModule(request, (err, source) => (err ? reject(err) : resolve(source))); // TODO sourceMap, module
-    });
+  private async getVDom(): Promise<string> {
+    return await this.loadWith("sfcLoader?type=template");
   }
 
-  private async getVDom(): Promise<string> {
+  private async loadWith(...loaders: string[]): Promise<string> {
     return new Promise((resolve, reject) => {
-      const request = `!sfcLoader?type=template!${this.loader.resourcePath}`;
-      this.loader.loadModule(request, (err, source) => (err ? reject(err) : resolve(source))); // TODO sourceMap, module
+      const request = `!${loaders.join("!")}!${this.loader.resourcePath}`;
+      this.loader.loadModule(request, (err, source) => (err ? reject(err) : resolve(source)));
     });
   }
 }
