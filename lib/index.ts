a88d9cc72d5573739163c03903776566838a92bc
Lukas Bombach <lukas.bombach@googlemail.com>
2019-03-26 14:06:28 +0100

    more code cleanup

--- lib/index.ts
+++ lib/index.ts
@@ -1,54 +1,15 @@
-import { loader } from "webpack";
 import { parseQuery } from "loader-utils";
 import Template from "./transpiler/react/template";
 import Script from "./transpiler/react/script";
-import Style from "./transpiler/react/style";
 import Component from "./transpiler/react/component";
 
-const sfcLoader /* : loader.Loader */ = function(source: string) {
+export default async function(source: string): Promise<string> {
   const type = this.query ? parseQuery(this.query).type : undefined;
-  // if (type === "template") return new Template(this, source).toString();
-  // if (type === "style") return new Style(this, source).toString();
-
-  //console.log("type", type, "\n", source);
-
   const callback = this.async();
 
-  if (type === "script") {
-    new Script(this, source)
-      .toString()
-      .then(str => {
-        callback(null, str);
-      })
-      .catch(err => {
-        console.log(err);
-        callback(err);
-      });
-  } else if (type === "template") {
-    new Template(this, source)
-      .toString()
-      .then(str => {
-        callback(null, str);
-      })
-      .catch(err => {
-        console.log(err);
-        callback(err);
-      });
-  } else {
-    // console.log(source);
-    new Component(this, source)
-      .toString()
-      .then(str => {
-        console.log(str);
-        callback(null, str);
-      })
-      .catch(err => {
-        console.log(err);
-        callback(err);
-      });
-  }
+  if (type === "template") callback(null, await new Template(this, source).toString());
+  if (type === "script") callback(null, await new Script(this, source).toString());
+  if (type === undefined) callback(null, await new Component(this, source).toString());
 
   return undefined;
-};
-
-export default sfcLoader;
+}
--- lib/transpiler/react/script.ts
+++ lib/transpiler/react/script.ts
@@ -4,7 +4,6 @@ import Component from "../../parser/component";
 export default class ScriptTranspiler extends Transpiler {
   async toString() {
     const content = Component.script(this.source);
-    // console.log(content);
     this.loader.resourcePath += ".js";
     return content;
   }
